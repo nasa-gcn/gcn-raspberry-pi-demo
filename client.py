b932e882619260d005b84aa1c6e5c23e9e138973
Leo Singer <leo.singer@ligo.org>
2023-12-14 17:19:45 -0500

    Optimize screen real estate in client script

--- client.py
+++ client.py
@@ -5,54 +5,56 @@ from uuid import uuid4
 import signal
 
 import confluent_kafka
-from curtsies.fmtfuncs import bold, fmtstr
+from rich.console import Console
+from rich.text import Text
 import gpiozero
 import randomname
 import typer
 
-COLORS = {'red', 'green', 'blue'}
+COLORS = {"red", "green", "blue"}
 
 
 def main(
-        bootstrap_server: Annotated[str, typer.Option()],
-        color: Annotated[str, typer.Option()],
+    bootstrap_server: Annotated[str, typer.Option()],
+    color: Annotated[str, typer.Option()],
 ):
-    consumer = confluent_kafka.Consumer({
-        'bootstrap.servers': bootstrap_server,
-        'group.id': str(uuid4()),
-        'enable.auto.commit': False,
-        'socket.timeout.ms': 1000,
-        'log_level': 0,
-    })
-    producer = confluent_kafka.Producer({
-        'bootstrap.servers': bootstrap_server,
-        'socket.timeout.ms': 1000,
-        'log_level': 0,
-    })
-    topics = list(COLORS - {color})
+    console = Console()
+    consumer = confluent_kafka.Consumer(
+        {
+            "bootstrap.servers": bootstrap_server,
+            "group.id": str(uuid4()),
+            "enable.auto.commit": False,
+            "socket.timeout.ms": 1000,
+            "log_level": 0,
+        }
+    )
+    producer = confluent_kafka.Producer(
+        {
+            "bootstrap.servers": bootstrap_server,
+            "socket.timeout.ms": 1000,
+            "log_level": 0,
+        }
+    )
     consumer.subscribe(list(COLORS - {color}))
 
     def consume():
         while True:
             for message in consumer.consume():
                 topic = message.topic()
-                text = fmtstr(
-                    bold(f'[{topic}]') + ' consumed "'
-                    + (message.error() or message.value().decode()) + '"',
-                    style=topic)
-                print(text)
+                console.print(
+                    f'[{topic} bold]RECV[not bold] {message.error() or message.value().decode()}'
+                )
 
     threading.Thread(target=consume, daemon=True).start()
 
     def produce():
-        value = randomname.get_name()
-        text = fmtstr(
-            bold(f'[{color}]') + ' produced "' + value + '"',
-            style=color)
+        value = '-' * 20
+        while len(value) > 10:
+            value = randomname.get_name()
+        console.print(f'[{color} bold]SEND[not bold] {value}')
         producer.produce(color, value)
-        print(text)
 
-    button = gpiozero.Button(21, bounce_time=0.0001, hold_time=0.5, hold_repeat=True)
+    button = gpiozero.Button(21, bounce_time=0.001, hold_time=0.5, hold_repeat=True)
     button.when_activated = button.when_held = produce
 
     signal.pause()
