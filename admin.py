31381773c5795b0ac43ef18011854a8f2bf56ff7
Leo Singer <leo.singer@ligo.org>
2023-12-15 09:40:49 -0500

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
-from time import sleep
 from typing import Annotated
 
-from rich.live import Live
-from rich.table import Table
-from rich.text import Text
 import confluent_kafka.admin
+from rich.table import box, Table
+from rich.live import Live
+from rich.text import Text
 import typer
 
-COLORS = ['red', 'green', 'blue']
+COLORS = ["red", "green", "blue"]
+symbols = "?NY"
 
 
 def main(
@@ -17,28 +17,42 @@ def main(
     admin = confluent_kafka.admin.AdminClient({
         'bootstrap.servers': bootstrap_server,
         'socket.timeout.ms': 1000,
-        'log_level': 0,
+        # 'log_level': 0,
     })
 
     topic_collection = confluent_kafka.TopicCollection(COLORS)
 
-    table = Table(title='In Sync Replicas')
-    table.add_column('Topic')
-    for i in range(1, 4):
-        table.add_column(f'Broker {i}')
-    cells = []
-    for topic in COLORS:
-        row = [Text() for _ in range(3)]
-        cells.append(row)
-        table.add_row(f'[{topic}]{topic}', *row)
-    
-    with Live(table, refresh_per_second=4):
+    table = Table(
+        width=15,
+        title="Brokers",
+        title_justify="right",
+        caption="insync replicas",
+        caption_style='gray italic',
+        title_style="bold",
+        show_edge=False,
+        pad_edge=False,
+        collapse_padding=True,
+        box=box.SIMPLE_HEAD,
+    )
+    table.add_column("Topic")
+    table.add_column("1")
+    table.add_column("2")
+    table.add_column("3")
+    rows = [[Text() for _ in range(3)] for __ in range(3)]
+    for topic, row in zip(COLORS, rows):
+        table.add_row(f"[bold {topic}]{topic}", *row)
+
+    with Live(table):
         while True:
-            for topic, row, future in zip(COLORS, cells, admin.describe_topics(topic_collection).values()):
-                isr = {isr.id for isr in future.result().partitions[0].isr}
-                for i, cell in enumerate(row):
-                    cell.plain = 'X' if i + 1 in isr else ''
-            sleep(0.1)
+            for topic, row, future in zip(COLORS, rows, admin.describe_topics(topic_collection, request_timeout=0.5).values()):
+                try:
+                    isr = {isr.id for isr in future.result().partitions[0].isr}
+                except confluent_kafka.KafkaException:
+                    for cell in row:
+                        cell.plain = '?'
+                else:
+                    for i, cell in enumerate(row):
+                        cell.plain = 'Y' if i + 1 in isr else 'N'
 
 
 if __name__ == "__main__":
